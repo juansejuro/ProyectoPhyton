da50e8565b7370f3c3257fa176e3e4a1dd6bda17
juansejuro <juansejuro@hotmail.com>
2024-08-19 16:40:52 -0500

    culmina crud alumnos

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('login/', views.login, name='login'),
     path('alumnos', views.alumnos, name='alumnos'),
     path('<int:id>', views.view_alumno, name='view_alumno'),
     path('agregar/', views.alumno_agregar, name='alumno_agregar'),
--- app/views.py
+++ app/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from .models import Alumno
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
 
 # Create your views here.
+# def login(request):
+#     return render(request, 'app/login.html')
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('alumnos')
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
     return render(request, 'app/login.html')
 
 
@@ -22,32 +35,39 @@ def view_alumno(request, id):
 
 def alumno_agregar(request):
     if request.method == 'POST':
-        alumno=Alumno(nombre=request.POST["nombre"],
-                    apellidos=request.POST["apellidos"],
-                    edad=request.POST["edad"],
-                    correo=request.POST["correo"],
-                    numDoc=request.POST["numDoc"],)
+        alumno=Alumno(
+            nombre=request.POST["nombre"],
+            apellidos=request.POST["apellidos"],
+            edad=request.POST["edad"],
+            correo=request.POST["correo"],
+            numDoc=request.POST["numDoc"],
+            usuarioCreacion=request.user,
+            estado=request.POST["estado"]
+            )
         # grabar producto en la BD
         alumno.save()
-        return redirect('/')
+        return redirect('alumnos')
     else:
-        return render(request, 'app/alumnos.html',{'alumnos': Alumno.objects.all()})
+        return render(request, 'app/alumno_agregar.html')
     
 def alumno_editar(request, id):
+    #alumno = Alumno.objects.get(id=id)
+    alumno = get_object_or_404(Alumno, id=id)
     if request.method == 'POST':
         alumno.nombre=request.POST["nombre"]
         alumno.apellidos=request.POST["apellidos"]
         alumno.edad=request.POST["edad"]
         alumno.correo=request.POST["correo"]
         alumno.numDoc=request.POST["numDoc"]
+        alumno.usuarioCreacion=request.user    
+        alumno.estado=request.POST["estado"]
         alumno.save()
-        return redirect('/')
+        return redirect('alumnos')
     else:
-        alumno = Alumno.objects.get(id=id)
-        return render(request, 'app/alumno_editar.html',{'alumno': alumno})
+        return render(request, 'app/alumno_editar.html', {'alumno': alumno})
     
 def alumno_eliminar(request, id):
     alumno = Alumno.objects.get(id=id)
     alumno.delete()
-    return redirect('/')
+    return redirect('alumnos')
 
